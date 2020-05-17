d540f8601dc58a31478042cd5a9a55a4f8e46066
cartograflow <13615192+fbahoken@users.noreply.github.com>
2020-05-17 19:33:08 +0200

    Update 1.0.1
    Compatibility with sf objects is ok

--- R/flowgini.R
+++ R/flowgini.R
@@ -1,20 +1,18 @@
 #' @title Analysis of flow concentration (Gini coefficient)
 #' @description
 #' Calculates Gini coefficient, plot Lorenz curve and
-#' threshold the matrice according to a global concentration criterion for mapping flow intensity or flow density).\cr
+#' threshold the matrice according to a global concentration criterion
+#' for mapping flow intensity or flow density.\cr
 #' To be use before \link{flowanalysis}
-#' @param tab dataset is a matrice or long format
-#' @param format is a variable that identify the data : matrice or long format
-#' @param origin origin place to be used with the long format
-#' @param dest destination place to be used with the list format
-#' @param valflow to be used with the list format
-#' @param fdc is the map background file, ie. a shapefile.
-#' @param code is the map background IDs code
-#' @param lorenz.plot allows to plot the Lorenz curve associate to the gini coefficient
+#' @param ODpts the input  dataset with : nodes code, flow values and XY coordinates
+#' @param origin ID origin place, in long format
+#' @param destination ID destination place, long format
+#' @param valflow flow value between origin and destination places
+#' @param lorenz.plot to plot or the Lorenz curve. See Details
 #' @return plot Lorenz curve for the cumulated flow and links : flowgini(...,gini.plot = TRUE),warning : the function must be not assign a variable
 #' @return value of the Gini's coefficent and the table : table<-flowgini(...,missing(gini.plot) or gini.plot = FALSE )
 #' @details
-#' flowgini(...,lorenz.plot = TRUE) for ploting Lorenz curve from cumulated flows and links.
+#' flowgini(...,lorenz.plot = TRUE) for ploting Lorenz curve  associate to the gini coefficient, from cumulated flows and links.
 #' @rawNamespace import(plotly, except = last_plot)
 #' @importFrom ggplot2 ggplot
 #' @importFrom ggplot2 aes
@@ -38,98 +36,61 @@
 #' @examples
 #' library(cartograflow)
 #' data(flowdata)
-#' bkg<- system.file("shape/MGP_TER.shp", package="cartograflow",
-#'                   lib.loc = NULL, mustWork = TRUE)
-#' #Computes Gini's coefficent
-#' tab_gini<-flowgini(flows,format="L",origin="i",dest="j",valflow="Fij",
-#'           bkg,code="EPT_NUM",lorenz.plot = FALSE)
-#' #Plot Lorenz curve
-#' flowgini(tab_gini,format="L",origin="i",dest="j",valflow="ydata",
-#'           bkg,code="EPT_NUM",lorenz.plot = TRUE)
-#' #See \link{flowanalysis} for viewing the tab_gini table
+#' # Computes Gini's coefficent
+#' tabgini <- flowgini(ODpts = flows, origin = "i", destination = "j", 
+#'                      valflow = "Fij", lorenz.plot = FALSE)
+#' # Plot Lorenz curve
+#' flowgini(ODpts = flows, origin = "i", dest = "j", valflow = "Fij", lorenz.plot = TRUE)
+#' # See \link{flowanalysis} for viewing the tab_gini table
 #' @export
 
-flowgini<-function(tab, origin, dest, valflow,format,
-                   fdc,code,lorenz.plot){
-
-  if(missing(tab) && missing(format) && missing(fdc) && missing (code))
-    stop("one of 'tab' and 'format' and 'fdc' and 'code' must be given")
-
-  gini<-function(vec1,vec2){
-    tot<-vec2[1]/2*vec1[1]
-    i<-2
-    while(i <=length(vec1))
-    {tot<-tot+(vec1[i]-vec1[i-1])*(vec2[i]+vec2[i-1])/2
-    i<-i+1
+flowgini <- function(ODpts, origin, destination, valflow, lorenz.plot) {
+  gini <- function(vec1, vec2) {
+    tot <- vec2[1] / 2 * vec1[1]
+    i <- 2
+    while (i <= length(vec1)) {
+      tot <- tot + (vec1[i] - vec1[i - 1]) * (vec2[i] + vec2[i - 1]) / 2
+      i <- i + 1
     }
-    res<-2*(0.5-tot)
-    return(res)}
+    res <- 2 * (0.5 - tot)
+    return(res)
+  }
 
-  ginigraph<-function(x,y){
-    p<-ggplot(x) +
-      geom_line(aes(x =x$flowcum, y = x$linkcum )) +
-      geom_line(aes(x =x$flowcum, y = x$flowcum )) +
+  ginigraph <- function(x, y) {
+    p <- ggplot(x) +
+      geom_line(aes(x = x$flowcum, y = x$linkcum)) +
+      geom_line(aes(x = x$flowcum, y = x$flowcum)) +
       xlab("Cumulative links") + ylab("Cumulative flows") +
-      ggtitle(paste("Gini's coefficent =",round(y*100,2)," %")) +
+      ggtitle(paste("Gini's coefficent =", round(y * 100, 2), " %")) +
       theme(
         panel.background = element_blank(),
         panel.grid.minor = element_blank(),
         panel.grid.major = element_line(color = "gray50", size = 0.5),
         panel.grid.major.x = element_blank(),
-        axis.text.y = element_text(colour="#68382C", size = 9))
+        axis.text.y = element_text(colour = "#68382C", size = 9)
+      )
     ggplotly(p) %>% layout(dragmode = "select")
   }
 
-  gini.tab<-function(g.tab,fdc,code){
-    gini.tab<-flowjointure(g.tab,fdc,code)
-    gini.tab$link<-1
-    gini.tab<-gini.tab[gini.tab$ydata>0,]
-    gini.tab<-gini.tab[order(gini.tab$ydata,decreasing=TRUE),]
-    gini.tab$flowcum<-cumsum(gini.tab$ydata)/sum(gini.tab$ydata)
-    gini.tab$linkcum<-cumsum(gini.tab$link)/sum(gini.tab$link)
-    return(gini.tab)}
-
-  if(format == "M"){
-    tabflow<-flowstructmat(tab)
-    tabflow<-flowtabmat(tabflow,matlist ="L")
-    tabgini<-gini.tab(tabflow,fdc,code)
+  gini.tab <- function(g.tab) {
+    gini.tab <- g.tab
+    gini.tab$link <- 1
+    gini.tab <- gini.tab[gini.tab[, valflow] > 0, ]
+    gini.tab <- gini.tab[order(gini.tab[, valflow], decreasing = TRUE), ]
+    gini.tab$flowcum <- cumsum(gini.tab[, valflow]) / sum(gini.tab[, valflow])
+    gini.tab$linkcum <- cumsum(gini.tab$link) / sum(gini.tab$link)
+    return(gini.tab)
   }
 
-  if(format == "L"){
-    tabmap <- tab %>%
-      select(origin, dest, valflow)
-    names(tabmap) = c("i", "j", "ydata")
+  tabgini <- gini.tab(ODpts)
 
-    tabgini<-gini.tab(tabmap,fdc,code)
-    }
+  indice <- gini(tabgini$flowcum, tabgini$linkcum)
 
-  indice<-gini(tabgini$flowcum,tabgini$linkcum)
-
-  if(missing(lorenz.plot) || lorenz.plot == FALSE){
-    message("Gini's coefficent =",paste(round(indice*100,2),"%"),"\n")
-    return(tabgini)}
-  if (lorenz.plot == TRUE){ginigraph(tabgini,indice)}
-
+  if (missing(lorenz.plot) || lorenz.plot == FALSE) {
+    message("Gini's coefficent =", paste(round(indice * 100, 2), "%"), "\n")
+    return(tabgini)
+  }
+  if (lorenz.plot == TRUE) {
+    ginigraph(tabgini, indice)
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
