8df2106609444a5c76b8457599e23cefee4df47c
cartograflow <13615192+fbahoken@users.noreply.github.com>
2020-05-17 19:25:34 +0200

    Update for 1.0.1
    Compatibility with sf objects is ok
    Add Manhattan distance

--- R/flowdist.R
+++ R/flowdist.R
@@ -1,49 +1,64 @@
-#' @title Compute continous distance matrix from geographical background
+#' @title Builds a continuous distance matrices from a spatial features background
 #' @description
-#' From a geographical background computes (and threshold) a distance matrix.
-#' @param tab the input flow dataset.
-#' @param dist.method euclidian calculation
-#' @param result take the vallue "flowdist" or "dist" allows to parameter the resulting distance dataset (flows filtered by a distance criterion or not)
-#' @return (1) A flowdata set with continuous euclidian distances calculations, see dist.method parameter
-#' @return (2) A flowdata set with movement from euclidian distances calculations.
-#' @return (3) A flowmap filtered by a global distance criterion.
+#' From a layer of areal spatial features, compute and threshold a continuous distance matrix.
+#' The result is either a matrice of distances between ODs, or a flow matrix based on the distance travelled between ODs ; both can be used for filtering flow before flow mapping (\link{flowmap})
+#' @param tab the input flow dataset
+#' @param dist.method distance calculation algorithm, default is euclidian calculation
+#' @param result Choose Building a "flowdist" or a simple "dist" matrice. See Details 
+#' @return (1) A flowdata set with continuous  distances calculations. See dist.method parameter
+#' @return (2) A flowdata set with movement from euclidian distances calculations
 #' @details
-#' -- result = "dist" is the resulting tab of the distance\cr
-#' -- result = "flowdist" with all the calculated parameters
+#' -- result = "dist" is the simple resulting distance matrice.\cr
+#' -- result = "flowdist" is the resulting distance matrice with additional calculated parameters.\cr
+#' -- It is also possible to filter flow by a level of distance travelled.
 #' @import dplyr
 #' @importFrom rlang .data
 #' @importFrom utils tail
 #' @examples
 #' library(cartograflow)
+#' library(sf)
 #' data(flowdata)
-#' bkg<- system.file("shape/MGP_TER.shp", package="cartograflow",
-#'                   lib.loc = NULL, mustWork = TRUE)
-#' tab<-flowjointure(flows,bkg,"EPT_NUM")
-#' #Format long with only origin, destination and distance parameters:
-#' tab.distance<-flowdist(tab, dist.method = "euclidian",result = "dist")
-#' #Format long with with all parameters: coordinates, distance, mouvement
-#' tab.distance<-flowdist(tab, dist.method = "euclidian",result = "flowdist")
+#' map <- st_read(system.file("shape/MGP_TER.shp", package = "cartograflow"))
+#' tabflow <- flowjointure(
+#'   geom = "area", bkg = map, DF.flow = flows, origin = "i",
+#'   destination = "j", id = "EPT_NUM", x = "X", y = "Y"
+#' )
+#'
+#' # Format long with only origin, destination and distance parameters:
+#' tab.distance <- flowdist(tabflow, dist.method = "euclidian", result = "dist")
+#' # Format long with with all parameters: coordinates, distance, mouvement
+#' tab.distance <- flowdist(tabflow, dist.method = "euclidian", result = "flowdist")
 #' @export
 
-flowdist<-function(tab,dist.method,result){
+flowdist <- function(tab, dist.method, result) {
+  if (dist.method == "euclidian") {
+                tabflow <- tab %>%
+                          mutate(distance = sqrt((.data$Xi - .data$Xj)^2 + (.data$Yi - .data$Yj)^2)) %>%
+                          mutate(mouvement = .data$ydata * .data$distance)
 
-if (dist.method == "euclidian"){
-       tabflow<-tab %>%
-           mutate(distance=sqrt((.data$X1-.data$X2)^2+(.data$Y1-.data$Y2)^2)) %>%
-           mutate(mouvement =.data$ydata*.data$distance)
-
-       if (result == "flowdist"){
-             return(tabflow)}
-       if (result == "dist"){
-          tab.reduction<-select(tabflow,-.data$ydata,-.data$X1,-.data$Y1,-.data$X2,-.data$Y2,-.data$mouvement)
-          return(tab.reduction)
-       }
+                if (result == "flowdist") {
+                            return(tabflow)
+                }
+                
+                if (result == "dist") {
+                             tab.reduction<-tabflow %>% select( .data$i,.data$j,.data$distance)
+                             return(tab.reduction)
+                }
   }
 
-if (dist.method == "xxx"){}
-
+  if (dist.method == "manhattan") {
+               tabflow <- tab %>%
+                          mutate(distance = abs(.data$Xj- .data$Xi)+ abs(.data$Yj - .data$Yi)) %>%
+                          mutate(mouvement = .data$ydata * .data$distance) 
+              
+                if (result == "flowdist") {
+                            return(tabflow)
+                }
+               
+               if (result == "dist") {
+                          tab.reduction<-tabflow %>% select( .data$i,.data$j,.data$distance)          
+                          return(tab.reduction)
+               }
+    
+  }
 }
-
-
-
-
