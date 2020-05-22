1d036d976651b276413b364a494ed5b868c3db8e
cartograflow <13615192+fbahoken@users.noreply.github.com>
2020-05-22 22:13:28 +0200

    Add flowtype computations
    - Extension of the computation possibilities of flowtypes (for "L" format only). 
    - Add : "biasym", "bimin", "bimax", "birange", "bidisym"
    - Change name of "bisold" to "bibal"
    
    -
    #' - x = "bibal" for bilateral balance or net flow FBij=(Fij-Fji) \cr
    #' - x = "biasym" for asymetry of bilateral flow FSij=(FBij/FSij)\cr
    #' - x = "bimin" for minFij=(Fij, Fji)\cr
    #'- x = "bimax" for maxFij(Fij, Fji)\cr
    #'- x = "birange" for bilateral rangeFij=(maxFij - minFij)\cr
    #'- x = "bidisym" bilateral disymetry as FDij=(FSij/rangeFij)

--- R/flowtype.R
+++ R/flowtype.R
@@ -1,15 +1,25 @@
 #' @title Compute flowdata types (volum, balance)
 #' @description Compute gross (volumn) and net (balance) flows from initial asymetric flow values
 #' @param tab is the input flow dataset
+#' @param origin the place of origin code
+#' @param destination the place of destination code
+#' @param fij the flow value between origin and destination places
 #' @param format specify the flow dataset format, "M " for square matrix [n*n] or L for long [i,j,data]
-#' @param x enter the computation type : "flux", "transpose", "bivolum" and "bisold".
+#' @param x enter the flow indicator type : "alltypes", "flux", "transpose", "bivolum", "bibal","biasym","bimin", "bimax","birange" and "bidisym"
 #' @details The matrice must be squared (if not, see \link{flowcarre}).
 #' This function compute for all pairs or origin-destination places (i,j)
 #' involved in an asymetric flow matrix (Fij<> Fji) several matrix :\cr
 #' - x = "flux" for remaining initial flow (Fij)\cr
 #' - x = "transpose" for reverse flow value (Fji)\cr
-#' - x = "bivolum" for bilateral gross flow Vij=(Fij+Fji)\cr
-#' - x = "bisold" for bilateral net flow Sij=(Fij-Fji) \cr
+#' - x = "bivolum" for bilateral volum as gross flow FSij=(Fij+Fji)\cr
+#' - x = "bibal" for bilateral balance or net flow FBij=(Fij-Fji) \cr
+#' - x = "biasym" for asymetry of bilateral flow FSij=(FBij/FSij)\cr
+#' - x = "bimin" for minFij=(Fij, Fji)\cr
+#'- x = "bimax" for maxFij(Fij, Fji)\cr
+#'- x = "birange" for bilateral rangeFij=(maxFij - minFij)\cr
+#'- x = "bidisym" bilateral disymetry as FDij=(FSij/rangeFij) 
+#' @import dplyr
+#' @importFrom rlang .data
 #' @examples
 #' library(cartograflow)
 #' data(flowdata)
@@ -23,15 +33,14 @@
 #' m <- flowtype(matflow, format = "M", x = "flux")
 #' m <- flowtype(matflow, format = "M", x = "transpose")
 #' m <- flowtype(matflow, format = "M", x = "bivolum")
-#' m <- flowtype(matflow, format = "M", x = "bisold")
+#' m <- flowtype(matflow, format = "M", x = "bibal")
 #'
 #' ## 1b:Computes flowtypes: Long format
-#' list <- flowtabmat(matflow, matlist = "L")
-#' colnames(list) <- c("i", "j", "Fij")
-#' l_all <- flowtype(list, format = "L", x = "all")
-#' l_sold <- flowtype(list, format = "L", x = "bisold")
+#' list <- flowtabmat(tabflow, matlist = "L")
+#' l_all <- flowtype(list,origin ="i",destination="j",fij="Fij", format = "L", x = "alltypes")
+#' l_sold <- flowtype(list, origin ="i",destination="j",fij="Fij",format = "L", x = "bibal")
 #' \donttest{
-#' # 2:flowmapping: example of bisold
+#' # 2:flowmapping: example of bibal
 #' flowmap(l_sold,
 #'   format = "L", bkg, code = "EPT_NUM",
 #'   filter = TRUE, threshold = 20, taille = 5
@@ -43,71 +52,114 @@
 #' number 116, URL : https://mappemonde-archive.mgm.fr/num44/articles/art14404.html
 #' @export
 
-flowtype <- function(tab, format, x) {
+flowtype <- function(tab,origin=NULL,destination=NULL,fij=NULL,format, x) {
+  
   if (format == "M") {
-    if (nrow(tab) != ncol(tab)) {
-      warning("your matrix is not square")
-    }
-
-    if (x == "flux") {
-      return(tab)
-    }
-
-    if (x == "transpose") {
-      fji <- t(tab)
-      return(fji)
-    }
-
-    if (x == "bivolum") {
-      fji <- t(tab)
-      vij <- tab + fji
-      return(vij)
-    }
-
-    if (x == "bisold") {
-      fji <- t(tab)
-      sij <- tab - fji
-      return(sij)
-    }
+    
+              if (nrow(tab) != ncol(tab)) {
+                warning("your matrix is not square")
+              }
+          
+              if (x == "flux") {
+                return(tab)
+              }
+          
+              if (x == "transpose") {
+                fji <- t(tab)
+                return(fji)
+              }
+          
+              if (x == "bivolum") {
+                fji <- t(tab)
+                vij <- tab + fji
+                return(vij)
+              }
+          
+              if (x == "bibal") {
+                fji <- t(tab)
+                sij <- tab - fji
+                return(sij)
+              }
+              
+              if(x== "bibal" || x== "biasym" || x== "bimin" || x == "bmax" || x == "birange" ){
+                message("This flow type is only available with the L format")
+              }
+    
   }
+  
   if (format == "L") {
-    f1 <- data.frame(tab$i, tab$j, tab$Fij)
+    maxFij<-Fij<-Fji<-NULL 
+    minFij<-Fij<-Fji<-NULL 
+    
+    f1 <- tab %>% select(origin,destination,fij)
     names(f1) <- c("i", "j", "Fij")
-    f2 <- data.frame(tab$j, tab$i, tab$Fij)
+   
+    f2<-tab %>% select(destination,origin,fij)
     names(f2) <- c("i", "j", "Fji")
+    
     tabflow <- merge(f1, f2, by = c("i", "j"), all.X = TRUE, all.Y = TRUE)
-    tabflow$FSij <- tabflow$Fij + tabflow$Fji
-    tabflow$FDij <- tabflow$Fji - tabflow$Fij
 
-    if (missing(x)) {
-      message("renseigner un choix de calcul : all, flux, transpose, solde, volume")
-    }
-    if (x == "all") {
-      return(tabflow)
-    }
+    tabflow<- tabflow %>% 
+              mutate(FSij = .data$Fij + .data$Fji)%>% 
+              mutate(FBij = .data$Fji - .data$Fij) %>% 
+              mutate(FAij = .data$FBij/ .data$FSij) %>% 
+              mutate(minFij = ifelse(.data$Fij < .data$Fji, .data$Fij, .data$Fji)) %>%
+              mutate(maxFij = ifelse(.data$Fij > .data$Fji, .data$Fij, .data$Fji)) %>%
+              mutate(rangeFij = maxFij - minFij) %>%
+              mutate(FDij = (.data$rangeFij)/.data$FSij)
+    
 
-    if (x == "flux") {
-      result <- data.frame(tabflow$i, tabflow$j, tabflow$Fij)
-      names(result) <- c("i", "j", "Fij")
-      return(result)
-    }
-
-    if (x == "transpose") {
-      result <- data.frame(tabflow$i, tabflow$j, tabflow$Fji)
-      names(result) <- c("i", "j", "Fji")
-      return(result)
-    }
-
-    if (x == "bivolum") {
-      result <- data.frame(tabflow$i, tabflow$j, tabflow$FSij)
-      names(result) <- c("i", "j", "FSij")
-      return(result)
-    }
-
-    if (x == "bisold") {
-      result <- data.frame(tabflow$i, tabflow$j, tabflow$FDij)
-      names(result) <- c("i", "j", "FDij")
-      return(result)
-    }
-  }
+            if (missing(x)) {
+              message("You must specify a choice of flow computation : alltypes, flux, transpose, bivolum ...")
+            }
+            if (x == "alltypes") {
+              return(tabflow)
+            }
+        
+            if (x == "flux") {
+              tabflow <- tabflow %>% select(.data$i,.data$j,.data$Fij)
+              return(tabflow)
+            }
+        
+            if (x == "transpose") {
+              tabflow <- tabflow %>% select(.data$i,.data$j,.data$Fji)
+              return(tabflow)
+            }
+        
+            if (x == "bivolum") {
+              tabflow <- tabflow %>% select(.data$i,.data$j,.data$FSij)
+              return(tabflow)
+            }
+        
+            if (x == "bibal") {
+              tabflow <- tabflow %>% select(.data$i,.data$j,.data$FBij)
+              return(tabflow)
+            }
+            
+            if (x == "biasym") {
+              tabflow <- tabflow %>% select(.data$i,.data$j,.data$FAij)
+              return(tabflow)
+            }
+            
+            if (x == "bimin") {
+              tabflow <- tabflow %>% select(.data$i,.data$j,.data$minFij)
+              return(tabflow)
+            }
+            
+            if (x == "bimax") {
+              tabflow <- tabflow %>% select(.data$i,.data$j,.data$maxFij)
+              return(tabflow)
+            }
+            
+            if (x == "birange") {
+              tabflow <- tabflow %>% select(.data$i,.data$j,.data$rangeFij)
+              return(tabflow)
+            }
+            
+            if (x == "bidisym") {
+                tabflow <- tabflow %>% select(.data$i,.data$j,.data$FDij)
+                return(tabflow)
+              }
+    
+          }
 }
